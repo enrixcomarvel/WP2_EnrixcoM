154f683d7cd25c631f2fd7708ac12740b745916d
enrixxcomarvel <enrixcomgp@gmail.com>
2022-03-21 15:59:34 +0700

    menambahkan file

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Enrixco Marvel saya tinggal di daerah ragunan bukan di dalem ragunan ya,hobi saya ngopi";
+    }
+    
+}
