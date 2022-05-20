ad2bad362ee0889ab3e1412cb6e248ccfa5d6c61
enrixcomarvel <enrixcomgp@gmail.com>
2022-05-20 07:48:10 +0700

    menambahkan file pertemuan 9

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,8 @@ $autoload['packages'] = array('form_validation');
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array('form_validation', 'session', 
+'database');
 
 /*
 | -------------------------------------------------------------------
